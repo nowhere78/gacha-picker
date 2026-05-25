e21cb7dffc9c7af1d2a343747a3c04ee69ecdf92
nowhere78 <wide1002@gmail.com>
2026-05-25 11:53:45 +0900

    Add PWA support (manifest, icon, service worker)

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'gacha-picker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './icon.svg',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Jua&family=Pretendard:wght@400;700&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
