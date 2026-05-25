e320901096b6d4bb8f9c9e1e754627363e44f673
nowhere78 <wide1002@gmail.com>
2026-05-25 22:03:28 +0900

    feat: add PNG icons to PWA manifest and cache

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = 'gacha-picker-v1';
+const CACHE_NAME = 'gacha-picker-v2';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
   './icon.svg',
+  './images/icon-192.png',
+  './images/icon-512.png',
   './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://fonts.googleapis.com/css2?family=Jua&family=Pretendard:wght@400;700&display=swap'
